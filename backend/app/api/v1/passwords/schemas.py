9613ed5e6143b02252d348fbcfc774a4c047faf2
MrRufRUS <knight.dark2017@yandex.ru>
2026-04-10 00:11:51 +0300

    fix: AnyUrl to str fix

--- backend/app/api/v1/passwords/schemas.py
+++ backend/app/api/v1/passwords/schemas.py
@@ -1,9 +1,9 @@
-from pydantic import AnyHttpUrl, BaseModel
+from pydantic import BaseModel
 
 
 class PasswordCreateSchema(BaseModel):
     service_name: str
-    service_url: AnyHttpUrl | None = None
+    service_url: str | None = None
     login: str
     password: str
 
@@ -11,25 +11,25 @@ class PasswordCreateSchema(BaseModel):
 class PasswordPublicSchema(BaseModel):
     id: int
     service_name: str
-    service_url: AnyHttpUrl | None = None
+    service_url: str | None = None
 
 
 class PasswordDetailSchema(BaseModel):
     id: int
     service_name: str
-    service_url: AnyHttpUrl | None = None
+    service_url: str | None = None
     login: str
     password_hash: str
 
 
 class PasswordUpdateSchema(BaseModel):
     service_name: str | None = None
-    service_url: AnyHttpUrl | None = None
+    service_url: str | None = None
     login: str | None = None
     password: str | None = None
 
 
 class PasswordListQuery(BaseModel):
     service_name: str | None = None
-    service_url: AnyHttpUrl | None = None
+    service_url: str | None = None
     login: str | None = None
